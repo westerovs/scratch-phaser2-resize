a732b338d253cc3d8e102b68bee77a8ca9c0ecfe
westerovs@mail.ru <westerovs@mail.ru>
2023-05-30 13:12:30 +0300

    refactoring

---
+++ README.md
@@ -0,0 +1,8 @@
+# TEMPLATE
+*****
+scratch компонент на canvas
+
+🟢RUN:<br>
+https://westerovs.github.io/scratch/
+<br>
+<img src="cover.png">
--- src/js/modules/ScratchBlock.js
+++ src/js/modules/ScratchBlock.js
@@ -9,66 +9,71 @@ export default class ScratchBlock {
     this.key = key
     this.minAlphaRatio = minAlphaRatio
     this.disabled = false
-  
+
     this.sprite = this.game.make.image(0, 0, key)
     this.spritePos = spritePos
-    
+
     this.bitmapData = null
-    
+
     this.init()
   }
-  
+
   init() {
     this.bitmapData = this.game.make.bitmapData(1000, 1000)
     this.bitmapData.addToWorld(0, 0)
     // this.bitmapData.fill(0, 0, 0, 0.2)
-    
+
     this.#createBlock()
     window.addEventListener('resize', () => this.#resize())
     console.log('MAX_PIXELS:', this.#getAlphaRatio())
   }
-  
+
   #createBlock = () => {
     if (window.matchMedia('(orientation: portrait)').matches) {
       this.sprite.position.set(this.spritePos.portrait.x, this.spritePos.portrait.y)
     }
-  
+
     if (window.matchMedia('(orientation: landscape)').matches) {
       this.sprite.position.set(this.spritePos.landscape.x, this.spritePos.landscape.y)
     }
-    
+
     this.bitmapData.draw(this.sprite)
-  
+
     this.sprite.alpha = 0
     this.sprite.inputEnabled = true
     this.sprite.events.onInputOver.add(() => {
       this.game.scratchSignal.dispatch(this.sprite.key, this)
     })
   }
-  
+
   update() {
     if (this.disabled) return
     if (this.game.input.activePointer.isDown) {
       this.sprite.alpha = 0
-      
+
       this.#drawBlend()
       this.#checkWin()
     }
-    
+
+    this.#pointerUp()
+  }
+
+  #pointerUp = () => {
+    // если нужно восстанавливать целостность изображение, если оно стёрто меньше чем на minAlphaRatio
     if (this.game.input.activePointer.isUp) {
-      if (this.#getAlphaRatio() > this.minAlphaRatio) {
-        this.sprite.alpha = 1
-        
-        this.bitmapData.draw(this.sprite, this.sprite.x, this.sprite.y)
-      }
+      // if (this.#getAlphaRatio() > this.minAlphaRatio) {
+      //   this.sprite.alpha = 1
+      //
+      //   this.bitmapData.draw(this.sprite, this.sprite.x, this.sprite.y)
+      // }
     }
   }
-  
+
   recoveryBlock = () => {
     this.sprite.alpha = 1
     this.bitmapData.draw(this.sprite, this.sprite.x, this.sprite.y)
   }
-  
+
   #drawBlend = () => {
     const cursorX = this.game.input.worldX / this.game.factor
     const cursorY = this.game.input.worldY / this.game.factor
@@ -82,7 +87,7 @@ export default class ScratchBlock {
     this.bitmapData.dirty = true
     // }
   }
-  
+
   #getAlphaRatio = () => {
     const {ctx} = this.bitmapData
     let alphaPixels = 0
@@ -100,41 +105,41 @@ export default class ScratchBlock {
 
     return +(alphaPixels / (ctx.canvas.width * ctx.canvas.height)).toFixed(3)
   }
-  
+
   #checkWin = () => {
     if (this.#getAlphaRatio() < this.minAlphaRatio) {
       console.warn('WIN')
       this.destroy()
     }
   }
-  
+
   destroy = () => {
     this.disabled = true
     this.sprite.inputEnabled = false
     this.bitmapData.context.clearRect(this.sprite.x, this.sprite.y, this.sprite.width, this.sprite.height)
   }
-  
+
   getImageURL = (imgData, width, height) => {
     const newCanvas = document.createElement('canvas')
     const ctx = newCanvas.getContext('2d')
     newCanvas.width = width
     newCanvas.height = height
-    
+
     ctx.putImageData(imgData, 0, 0)
     return newCanvas.toDataURL() //image URL
   }
-  
+
   createCopyImage = (x, y, width, height) => {
     const imageData = this.bitmapData.ctx.getImageData(x, y, this.sprite.width, this.sprite.height)
-    
+
     const copyImage = new Image()
     copyImage.src = this.getImageURL(imageData, width, height)
-  
+
     return new Promise(resolve => {
       copyImage.addEventListener('load', () => resolve(copyImage))
     })
   }
-  
+
   #resize = () => {
     this.createCopyImage(this.sprite.x, this.sprite.y, this.sprite.width, this.sprite.height)
       .then((copyCropImage) => {
@@ -146,7 +151,7 @@ export default class ScratchBlock {
         if (window.matchMedia('(orientation: landscape)').matches) {
           this.sprite.position.set(this.spritePos.landscape.x, this.spritePos.landscape.y)
         }
-        
+
         this.bitmapData.draw(copyCropImage, this.sprite.x, this.sprite.y)
       })
   }
